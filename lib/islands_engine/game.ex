f1deaf74f376d992fe6cf77582cb79adbc3fe49d
ryan <ryanyogan@gmail.com>
2021-06-02 19:37:25 -0500

    boundary layer started definition

---
+++ lib/islands_engine/game.ex
@@ -0,0 +1,141 @@
+defmodule IslandsEngine.Game do
+  use GenServer
+
+  alias IslandsEngine.{Board, Coordinate, Guesses, Island, Rules}
+
+  @players [:player1, :player2]
+
+  def start_link(name) when is_binary(name) do
+    GenServer.start_link(__MODULE__, name, [])
+  end
+
+  def add_player(game, name) when is_binary(name) do
+    GenServer.call(game, {:add_player, name})
+  end
+
+  def position_island(game, player, island, row, col) when player in @players do
+    GenServer.call(game, {:position_island, player, island, row, col})
+  end
+
+  def set_islands(game, player) when player in @players do
+    GenServer.call(game, {:set_islands, player})
+  end
+
+  def guess_coordinate(game, player, row, col) when player in @players do
+    GenServer.call(game, {:guess_coordinate, player, row, col})
+  end
+
+  @impl true
+  def init(name) do
+    player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
+    player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
+
+    {:ok, %{player1: player1, player2: player2, rules: %Rules{}}}
+  end
+
+  @impl true
+  def handle_call({:add_player, name}, _from, state) do
+    with {:ok, rules} <- Rules.check(state.rules, :add_player) do
+      state
+      |> update_player2_name(name)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error ->
+        {:reply, :error, state}
+    end
+  end
+
+  @impl true
+  def handle_call({:position_island, player, island_type, row, col}, _from, state) do
+    board = player_board(state, player)
+
+    with {:ok, rules} <- Rules.check(state.rules, {:position_islands, player}),
+         {:ok, coordinate} <- Coordinate.new(row, col),
+         {:ok, island} <- Island.new(island_type, coordinate),
+         %{} = board <- Board.position_island(board, island_type, island) do
+      state
+      |> update_board(player, board)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error ->
+        {:reply, :error, state}
+
+      {:error, :invalid_coordinate} ->
+        {:reply, {:error, :invalid_coordinate}, state}
+
+      {:error, :overlapping_island} ->
+        {:reply, {:error, :overlapping_island}, state}
+
+      {:error, :invalid_island_type} ->
+        {:reply, {:error, :invalid_island_type}, state}
+    end
+  end
+
+  @impl true
+  def handle_call({:set_islands, player}, _from, state) do
+    board = player_board(state, player)
+
+    with {:ok, rules} <- Rules.check(state.rules, {:set_islands, player}),
+         true <- Board.all_islands_positioned?(board) do
+      state
+      |> update_rules(rules)
+      |> reply_success({:ok, board})
+    else
+      :error ->
+        {:reply, :error, state}
+
+      false ->
+        {:reply, {:error, :not_all_islands_positioned}, state}
+    end
+  end
+
+  @impl true
+  def handle_call({:guess_coordinate, player_key, row, col}, _from, state) do
+    opponent_key = opponent(player_key)
+    opponent_board = player_board(state, opponent_key)
+
+    with {:ok, rules} <- Rules.check(state.rules, {:guess_coordinate, player_key}),
+         {:ok, coordinate} <- Coordinate.new(row, col),
+         {hit_or_miss, forested_island, win_status, opponent_board} <-
+           Board.guess(opponent_board, coordinate),
+         {:ok, rules} <- Rules.check(rules, {:win_check, win_status}) do
+      state
+      |> update_board(opponent_key, opponent_board)
+      |> update_guesses(player_key, hit_or_miss, coordinate)
+      |> update_rules(rules)
+      |> reply_success({hit_or_miss, forested_island, win_status})
+    else
+      :error ->
+        {:reply, :error, state}
+
+      {:error, :invalid_coordinate} ->
+        {:reply, {:error, :invalid_coordinate}, state}
+    end
+  end
+
+  defp update_player2_name(state, name),
+    do: put_in(state.player2.name, name)
+
+  defp update_rules(state, rules),
+    do: %{state | rules: rules}
+
+  defp update_board(state, player, board),
+    do: Map.update!(state, player, fn player -> %{player | board: board} end)
+
+  defp update_guesses(state, player_key, hit_or_miss, coordinate) do
+    update_in(state[player_key].guesses, fn guesses ->
+      Guesses.add(guesses, hit_or_miss, coordinate)
+    end)
+  end
+
+  defp reply_success(state, reply),
+    do: {:reply, reply, state}
+
+  defp player_board(state, player),
+    do: Map.get(state, player).board
+
+  defp opponent(:player1), do: :player2
+  defp opponent(:player2), do: :player1
+end
